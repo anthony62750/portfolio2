27007f32f8fcb2a10c75a8268697f0b20eb93a47
allard.anthony@hotmail.fr <allard.anthony@hotmail.fr>
2023-08-03 16:22:42 +0200

    new portfolio

---
+++ components/nav.php
@@ -0,0 +1,20 @@
+<header>
+    <nav>
+            <a href="/" id="logo">TOTO</a>
+            <i class="fas fa-bars" id="ham-menu"></i>
+            <ul id="nav-bar">
+                <li>
+                    <a href="../page/a-propos.php">a propos</a>
+                </li>
+                <li>
+                    <a href="../page/competence.php">compétences</a>
+                </li>
+                <li>
+                    <a href="../page/projets.php">Projets</a>
+                </li>
+                <li>
+                    <a href="../page/contact.php">Contact</a>
+                </li>
+            </ul>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="./assets/css/reset/reset.css"> -->
+    <link rel="stylesheet" href="./assets/css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <!-- mise en place de la police  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital@1&display=swap" rel="stylesheet">
+    <title>Document</title>
+
+</head>
+
+
+<body>
+    <?php include('./components/nav.php') ?>
+    <section>
+        <h2>A propos</h2>
+    </section>
+     <script src="./assets/js/index.js"></script>
+</body>
+</html>
